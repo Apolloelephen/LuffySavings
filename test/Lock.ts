794214665ff1b44d61368fa9d98ac8316faf48f2
apollo elephen <apollosphinx11@gmail.com>
2024-03-20 15:44:03 +0000

    first commit

---
+++ test/Lock.ts
@@ -0,0 +1,9 @@
+// import ///<references types ="ethers">
+import{ loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers"
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe ("SavingContract ", () => {
+    const deploySavingContract = async () => {}
+
+})
